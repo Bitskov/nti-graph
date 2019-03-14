4b232661add33ca478aa1fba1aa3788719f852d0
bitskoff <bitskoff@gmail.com>
2019-03-14 14:37:52 +0800

    Update #3

--- server-server.py
+++ server-server.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 
 @app.errorhandler(406)
 def error406(e):
-    return "Wrong content-type. \"Application\JSON must be sent\""
+    resp = make_response("Wrong content-type. \"Application\JSON must be sent\"")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
-    if request.headers['content-type'] != 'application/json':
+    if 'Content-Type' not in request.headers.keys() or request.headers['Content-Type'] != 'application/json':
         return abort(406)
 
     json = request.get_json()
     answer = simulate(json)
-    resp = (answer, 200)
+    #resp = (answer, 200)
+    resp = make_response(answer)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 
